3d7f5b34971347debd3a1163a5552cdb207058a3
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-06-04 21:38:58 +0530

    vercel build

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://learning-mangement-system-pi.vercel.app/",
-		credentials:true,
+		origin:"https://lms-server-git-main-sanjeevs-projects-2af8b8d1.vercel.app/api/v1",
+		
 	})
 )
 
