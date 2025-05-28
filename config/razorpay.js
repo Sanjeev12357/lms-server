95eb14a95fe5dc7b31ec9e416dc05262978fe9f2
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2025-05-28 17:38:22 +0530

    razorpay creds updated

--- config/razorpay.js
+++ config/razorpay.js
@@ -2,6 +2,6 @@ const Razorpay = require("razorpay");
 
 
 exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET,
+    key_id: "rzp_test_8D5DwZyENmZRU2",
+    key_secret: "WnGZpISItN3rrwkIwhgT6CyV",
 });
